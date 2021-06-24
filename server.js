9eefb0f2d9fa5463b87f5cfa084ff6198b6e7fb4
Anurag10300 <anurag.panwar.2001@gmail.com>
2021-06-24 18:43:27 +0530

    Skeleton

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const sql  = require("mysql");
 
 const app  = express();
 
+app.use(express.static(__dirname+'/public'));
+
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/login',function(req,res){
+    res.sendFile(__dirname+'/login.html');
 });
 
 app.listen(3000,function(){
